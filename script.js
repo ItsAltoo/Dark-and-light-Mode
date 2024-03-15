6007a68431924ffff0affea4afc9ce1f24cf604c
alto <itsmalik57@gmail.com>
2024-03-15 23:14:57 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let bg = document.getElementById("bg");
+
+const lightMode = () => {
+  bg.style.backgroundColor = "#fff";
+  bg.style.color = "#212121";
+};
+
+const darkMode = () => {
+  bg.style.backgroundColor = "#212121";
+  bg.style.color = "#fff";
+};
